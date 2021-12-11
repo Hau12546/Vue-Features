d41fe619454fd04bc772d265bbd0276d623cb2e5
Nguyễn Trọng Hậu <akitosama12546@gmail.com>
2021-12-11 22:03:36 +0700

    2021/12/11_DataBinding

---
+++ src/components/Assignment1.vue
@@ -0,0 +1,41 @@
+	<template>
+		<div class="card text-white bg-primary">
+				<!-- <img class="card-img-top" src="" alt=""> -->
+				<div class="card-body">
+						<h4 class="card-title">Name: {{UserInfo.name}}</h4>
+						<p class="card-text">Age: U{{UserInfo.age}}</p>
+						<p class="card-text">Next {{GetFutureYearsAge() - UserInfo.age}} Years: {{GetFutureYearsAge()}}</p>
+						<p class="card-text">Lucky Number: {{GetLuckyNumber()}}</p>
+						<div>
+								<input type="text" v-model="UserInfo.name" >
+						</div>
+				</div>
+		</div>
+		<img v-bind:src="avatar" class="img-fluid" alt="">
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+		data(){
+				return {
+						UserInfo:{
+							name:'HauNT',
+							age:23,
+							avatar:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt9DZKrj_ZJD2EPmds05DCPmPdXht9oG-PmYytmlbajeltPyRJXxhoepGbXsbwBjc6Cl0&usqp=CAU'
+						}
+				}
+		},
+		methods:{
+			GetFutureYearsAge(years=5){
+				return this.UserInfo.age+years
+			},
+			GetLuckyNumber(){
+				return Math.round((Math.random()*10)+1)
+			}
+		}
+}
+</script>
---
+++ src/components/Assignment2.vue
@@ -0,0 +1,44 @@
+<template>
+		<button class="btn btn-danger" v-on:click="ShowMessage()" type="button">Show Alert</button>
+		<div class="item-container">
+			<p>{{message}}</p>
+		</div>
+		<hr>
+		<input class="form-input" v-on:keydown="SetOutput1($event)">
+		<div class="item-container">
+			<p>{{output1}}</p>
+		</div>
+		<hr>
+		<input class="form-input" v-on:keyup.enter="SetOutput2($event)">
+		<div class="item-container">
+			<p>{{output2}}</p>
+		</div>
+</template>
+<style>
+
+</style>
+
+
+<script>
+export default {
+			data(){
+				return {
+					output1:'',
+					output2:'',
+					message:''
+				}
+			},
+			methods:{
+				ShowMessage(){
+					this.message = 'Question1 done'
+				},
+				SetOutput1(e){
+						this.output1 = e.target.value;
+				},
+
+				SetOutput2(e){
+						this.output2 = e.target.value;
+				},
+			},
+}
+</script>
